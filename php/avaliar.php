52b3ec47946482cc69641d4d17b8e0c09d18f2fd
Fuyuyukii <vinicius.aj.borges@gmail.com>
2023-05-18 12:24:26 -0300

    avaliacao braba

--- php/avaliar.php
+++ php/avaliar.php
@@ -2,9 +2,13 @@
     include("sql_connect.php");
     $produto_id = $_GET["produto_id"];
     $produto_avaliacao  = $_GET["produto_avaliacao"];
+    $cpf_usuario  = $_GET["cpf_usuario"];
 
-    $stmt = $pdo->prepare("INSERT INTO produto_avaliacao VALUES (:produto_id, :produto_avaliacao)");
+    $stmt = $pdo->prepare("INSERT INTO produto_avaliacao VALUES (:cpf_usuario, :produto_id, :produto_avaliacao)");
+    $stmt->bindParam(':cpf_usuario', $cpf_usuario);
     $stmt->bindParam(':produto_id', $produto_id);
     $stmt->bindParam(':produto_avaliacao', $produto_avaliacao);
     $stmt->execute();
+
+    header("Location: ../visualiza-produto/index.html");
 ?>
