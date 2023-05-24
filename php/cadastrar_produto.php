d973679e38a450c3ec0176887d19e9489e1c43b2
Fuyuyukii <vinicius.aj.borges@gmail.com>
2023-05-24 09:23:07 -0300

    coringando

--- php/cadastrar_produto.php
+++ php/cadastrar_produto.php
@@ -7,38 +7,37 @@
     $produto_info_tec = $_GET["npi"]; 
 
     
-    // INSERINDO OS DADOS BRUTOS
-    $stmt = $pdo->prepare("INSERT INTO produtos(nome, id_imagem, preco, categoria, descricao, info_tecnica)
-    VALUES (:nome_produto, :id_imagem,  :produto_preco, :produto_categoria, :produto_descricao, :produto_info_tec)");
-    $stmt->bindParam(':nome_produto', $nome_produto);
-    $stmt->bindParam(':id_imagem', $nome_produto);
-    $stmt->bindParam(':produto_preco', $produto_preco);
-    $stmt->bindParam(':produto_categoria', $produto_categoria);
-    $stmt->bindParam(':produto_descricao', $produto_descricao);
-    $stmt->bindParam(':produto_info_tec', $produto_info_tec);
-    $stmt->execute();
-    echo "Data inserted successfully";
+    // // INSERINDO OS DADOS BRUTOS
+    // $stmt = $pdo->prepare("INSERT INTO produtos(nome, preco, categoria, descricao, info_tecnica)
+    // VALUES (:nome_produto, :produto_preco, :produto_categoria, :produto_descricao, :produto_info_tec)");
+    // $stmt->bindParam(':nome_produto', $nome_produto);
+    // $stmt->bindParam(':produto_preco', $produto_preco);
+    // $stmt->bindParam(':produto_categoria', $produto_categoria);
+    // $stmt->bindParam(':produto_descricao', $produto_descricao);
+    // $stmt->bindParam(':produto_info_tec', $produto_info_tec);
+    // $stmt->execute();
+    // echo "Data inserted successfully";
 
     //PEGANDO O ID DO PRODUTO
     $stm = $pdo->prepare("SELECT MAX(id_produto) AS max_id FROM produtos");
     $stm->execute();
-    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    $row = $stm->fetch(PDO::FETCH_ASSOC);
     if (!$row) {
         die('No data found');
-    }
+    };
     $maxId = $row['max_id'];
 
     // INSERINDO AS IMAGENS
-    $targetDir = "../prodigy_imagens/product_imgs/";
+    $targetDir = "imgs/";
     $targetFile = $targetDir . basename($_FILES["photo"]["name"]);
-    $upload0k = 1;
+    $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-    if ($upload0k = 0){
-        echo "The img was not uploaded";
+    if ($uploadOk == 0) {
+        echo "The image was not uploaded.";
     } else {
-        if (move_uploaded_file($_FILES["photo"]["tmp_name"], $targetFile)){
-            echo "the file " . basename($_FILES["photo"]["name"]) . " has been uploaded.";
+        if (move_uploaded_file($_FILES["photo"]["tmp_name"], $targetFile)) {
+            echo "The file " . basename($_FILES["photo"]["name"]) . " has been uploaded.";
             $filePath = $targetFile;
             $stm = $pdo->prepare("INSERT INTO produtos_imagens (id_produto, file_path) VALUES (:id_produto, :filePath)");
             $stm->bindParam(':id_produto', $maxId);
