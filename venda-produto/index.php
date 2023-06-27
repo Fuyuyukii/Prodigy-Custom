30193b944af9848295405e618810490ceff8bb62
vodzinskylila <vodzinskyw@gmail.com>
2023-06-27 08:03:00 -0300

    arrumado o caminho das telas, diz o mestre

--- venda-produto/index.php
+++ venda-produto/index.php
@@ -26,7 +26,7 @@
         <nav class="nav navbar">
             <a href="../venda-produto/index.php"><img class="logo" src="fotologo.png" alt="imagem da logo"></a>
             <div class="icones">
-                <a href="../tela-carrinho/index.html">
+                <a href="../tela-carrinho/index.php">
                     <svg style="cursor:pointer" width="45px" height="45px" viewBox="0 0 24 24" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -59,7 +59,7 @@
 
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                         <li>
-                            <a href="../tela-minhaconta/index.html"
+                            <a href="../tela-minhaconta/index.php"
                                 class="dropdown-item d-flex align-items-center mt-2 mb-2 ps-3 gap-2">
                                 <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
@@ -79,7 +79,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="../tela-favoritos/index.html"
+                            <a href="../tela-favoritos/index.php"
                                 class="dropdown-item d-flex align-items-center mt-2 mb-2 ps-3 gap-2">
                                 <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
@@ -105,7 +105,7 @@
                                 $adm_verify = $linhas["adm"];
                                 if ($adm_verify == 1){
                                     echo(
-                                        "<a href='../tela-adm/index.html'
+                                        "<a href='../tela-adm/index.php'
                                         class='dropdown-item d-flex align-items-center mt-2 mb-2 ps-3 gap-2'>
                                         <svg width='20px' height='20px' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' fill='none'><g id='SVGRepo_bgCarrier' stroke-width='0'></g><g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g><g id='SVGRepo_iconCarrier'> <path stroke='#535358' stroke-linejoin='round' stroke-width='2' d='M13.905 3.379A.5.5 0 0114.39 3h3.22a.5.5 0 01.485.379l.689 2.757a.515.515 0 00.341.362c.383.126.755.274 1.115.443a.515.515 0 00.449-.003l2.767-1.383a.5.5 0 01.577.093l2.319 2.319a.5.5 0 01.093.577l-1.383 2.767a.515.515 0 00-.003.449c.127.271.243.549.346.833.053.148.17.265.319.315l2.934.978a.5.5 0 01.342.474v3.28a.5.5 0 01-.342.474l-2.934.978a.515.515 0 00-.32.315 9.937 9.937 0 01-.345.833.515.515 0 00.003.449l1.383 2.767a.5.5 0 01-.093.577l-2.319 2.319a.5.5 0 01-.577.093l-2.767-1.383a.515.515 0 00-.449-.003c-.271.127-.549.243-.833.346a.515.515 0 00-.315.319l-.978 2.934a.5.5 0 01-.474.342h-3.28a.5.5 0 01-.474-.342l-.978-2.934a.515.515 0 00-.315-.32 9.95 9.95 0 01-1.101-.475.515.515 0 00-.498.014l-2.437 1.463a.5.5 0 01-.611-.075l-2.277-2.277a.5.5 0 01-.075-.61l1.463-2.438a.515.515 0 00.014-.498 9.938 9.938 0 01-.573-1.383.515.515 0 00-.362-.341l-2.757-.69A.5.5 0 013 17.61v-3.22a.5.5 0 01.379-.485l2.757-.689a.515.515 0 00.362-.341c.157-.478.35-.94.573-1.383a.515.515 0 00-.014-.498L5.594 8.557a.5.5 0 01.075-.611l2.277-2.277a.5.5 0 01.61-.075l2.438 1.463c.152.091.34.094.498.014a9.938 9.938 0 011.382-.573.515.515 0 00.342-.362l.69-2.757z'></path> <circle cx='16' cy='16' r='5' stroke='#535358' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'></circle> </g></svg>
                                         <p style='margin:0'>Administração</p>
